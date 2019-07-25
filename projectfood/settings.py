a488f2a5b9c39ddbad1362c0ed96436f2fd1a284
Cesar <cesarsst@hotmail.com>
2019-07-25 11:07:31 -0300

    1
    1

--- projectfood/settings.py
+++ projectfood/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,3 +135,4 @@ AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
+DATABASES['default'] = dj_database_url.config()
