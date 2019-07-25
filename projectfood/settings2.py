db2de6fc8079537c3349fe03927eada60ebf9a7d
Cesar <cesarsst@hotmail.com>
2019-07-25 10:13:45 -0300

    1
    1

--- projectfood/settings2.py
+++ projectfood/settings2.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-import dj_database_url
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -137,5 +137,3 @@ LOGOUT_REDIRECT_URL = 'home'
 # Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals())
-
-DATABASES['default'] = dj_database_url.config()
